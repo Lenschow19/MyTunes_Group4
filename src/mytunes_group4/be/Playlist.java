73e8c5f6d224c9e115d1bed0a3bd6ddfc2024666
Lenschow19 <55918642+Lenschow19@users.noreply.github.com>
2019-11-26 10:21:33 +0100

    Finished up Song and Playlist class

--- src/mytunes_group4/be/Playlist.java
+++ src/mytunes_group4/be/Playlist.java
@@ -11,5 +11,26 @@ package mytunes_group4.be;
  */
 public class Playlist
 {
+    private String name; //name of playlist
+    private int totalDuration; //Displaying duration of all songs in the playlist
+    private int songCounter; //Displaying number of songs in the playlist
+    
+    public Playlist(String name, int totalDuration, int songCounter)
+    {
+        this.name = name;
+        this.totalDuration = totalDuration;
+        this.songCounter = songCounter;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+  
     
 }
--- src/mytunes_group4/be/Song.java
+++ src/mytunes_group4/be/Song.java
@@ -7,13 +7,53 @@ package mytunes_group4.be;
 
 /**
  *
- * @author Rizvan
+ * @author Rizvan & NLens
  */
 public class Song
 {
     private String songName;
     private String artistName;
-    private int duration;
     private String genre;
+    private int duration; //How long the songs playtime is
+    
+    
+    public Song(String songName, String artistName, String genre, int duration)
+    {
+        this.songName = songName;
+        this.artistName = artistName;
+        this.genre = genre;
+        this.duration = duration;
+    }
+
+    public String getSongName()
+    {
+        return songName;
+    }
+
+    public void setSongName(String songName)
+    {
+        this.songName = songName;
+    }
+
+    public String getArtistName()
+    {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName)
+    {
+        this.artistName = artistName;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
+    
     
 }
