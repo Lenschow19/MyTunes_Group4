5e2a9cfc23d1e2d34c29a42f4d09da2cb0d40ece
MMygind <56871472+MMygind@users.noreply.github.com>
2019-11-26 10:49:05 +0100

    Added DatabaseConnector
    Tilføjet DatabaseConnector

--- src/mytunes_group4/dal/database/DatabaseConnector.java
+++ src/mytunes_group4/dal/database/DatabaseConnector.java
@@ -5,11 +5,36 @@
  */
 package mytunes_group4.dal.database;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
 /**
  *
- * @author Rizvan
+ * @author mads_
  */
 public class DatabaseConnector
 {
+    private SQLServerDataSource dataSource;
+
+    public DatabaseConnector() throws IOException
+    {
+        Properties props = new Properties();
+        props.load(new FileReader("DBSettings.txt"));
+        dataSource = new SQLServerDataSource();
+        dataSource.setDatabaseName(props.getProperty("database"));
+        dataSource.setUser(props.getProperty("user"));
+        dataSource.setPassword(props.getProperty("password"));
+        dataSource.setServerName(props.getProperty("server"));
+    }
+
     
+    
+    public Connection getConnection() throws SQLServerException
+    {   
+        return dataSource.getConnection();
+    }
 }
