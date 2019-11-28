7b898eca8d67cdbbc85b5598d99a5fcb3099dd40
MMygind <56871472+MMygind@users.noreply.github.com>
2019-11-28 19:50:54 +0100

    Added CRUD Methods for Playlists and Made "AddPlaylists" window in GUI
    Tilføjet CRUD metoder til Playlister
    
    Ved at trykke "New" kan man nu åbne et nyt vindue, hvor der kan tilføjes playlister. "Create" metoden gør ikke noget endnu af ukendt grund.

--- src/mytunes_group4/gui/TunesModel.java
+++ src/mytunes_group4/gui/TunesModel.java
@@ -13,12 +13,15 @@ import mytunes_group4.be.Song;
 import mytunes_group4.bll.SongManager;
 import mytunes_group4.dal.DalException;
 import java.io.File;
+import java.util.Comparator;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import mytunes_group4.be.Playlist;
 import mytunes_group4.bll.MusicPlayer;
+import mytunes_group4.bll.PlaylistManager;
 
 /**
  *
@@ -28,7 +31,75 @@ public class TunesModel
 {
 
 
+    private PlaylistManager pm;
     private MusicPlayer mp;
+    
+    public TunesModel() throws IOException, DalException, Exception
+    {
+        this.pm = new PlaylistManager();
+        songManager = new SongManager();
+        allSongs = FXCollections.observableArrayList();
+        allSongs.addAll(songManager.getAllSongs());
+        
+        
+    }
+    
+    private ObservableList<Playlist> playlists = FXCollections.observableArrayList(); 
+    
+    public ObservableList<Playlist> getPlaylistList() throws IOException, Exception
+    {
+        playlists.addAll(pm.getAllPlaylists());
+        playlists.sort(new Comparator<Playlist>()
+            {
+                @Override
+                public int compare(Playlist arg0, Playlist arg1)
+                {
+                    return arg0.getId() - arg1.getId();
+                }
+
+            });
+        return playlists; 
+        
+    }
+    
+    public void updatePlaylist(Playlist selectedPlaylist) throws Exception
+    {
+        pm.updatePlaylist(selectedPlaylist);
+        if (playlists.remove(selectedPlaylist))
+        {
+            playlists.add(selectedPlaylist);
+            playlists.sort(new Comparator<Playlist>()
+            {
+                @Override
+                public int compare(Playlist arg0, Playlist arg1)
+                {
+                    return arg0.getId() - arg1.getId();
+                }
+
+            });
+        }
+    }
+    
+    public void deletePlaylist(Playlist selectedPlaylist) throws Exception
+    {
+        pm.deletePlaylist(selectedPlaylist);
+        playlists.remove(selectedPlaylist);
+    }
+    
+    public void createPlaylist(String name) throws Exception
+    {
+        Playlist playlist = pm.createPlaylist(name);
+        playlists.add(playlist);
+        playlists.sort(new Comparator<Playlist>()
+            {
+                @Override
+                public int compare(Playlist arg0, Playlist arg1)
+                {
+                    return arg0.getId() - arg1.getId();
+                }
+
+            });
+    }
 
    
 
@@ -49,14 +120,7 @@ public class TunesModel
     private ObservableList<Song> allSongs;
     private SongManager songManager;
 
-    public TunesModel() throws IOException, DalException
-    {
-        songManager = new SongManager();
-        allSongs = FXCollections.observableArrayList();
-        allSongs.addAll(songManager.getAllSongs());
-        
-        
-    }
+    
     
     public ObservableList<Song> getSongs()
     {
--- src/mytunes_group4/gui/TunesViewController.java
+++ src/mytunes_group4/gui/TunesViewController.java
@@ -1,15 +1,24 @@
 package mytunes_group4.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
 import javafx.scene.input.DragEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import mytunes_group4.be.*;
 
 /**
@@ -19,10 +28,8 @@ import mytunes_group4.be.*;
  */
 public class TunesViewController implements Initializable
 {
-    
-    private TunesModel tModel;
-    
 
+    private TunesModel tModel;
 
     @FXML
     private ListView<Playlist> Playlists;
@@ -34,41 +41,50 @@ public class TunesViewController implements Initializable
     @FXML
     private Slider volumeSlider;
 
-    
-
-    
-    
-
-
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        try {
-        tModel = new TunesModel();
-        SongList.setItems(tModel.getSongs());
+        try
+        {
+            tModel = new TunesModel();
+            SongList.setItems(tModel.getSongs());
         } catch (Exception ex)
         {
             System.out.println("Something went wrong");
             ex.printStackTrace();
         }
-       
+
+        try
+        {
+            Playlists.setItems(tModel.getPlaylistList());
+
+        } catch (Exception ex)
+        {
+            Logger.getLogger(TunesViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-
-        
-        
-        
-        
-                
-        
-       
-
 
     @FXML
     private void addNewPlaylist(ActionEvent event)
     {
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddPlaylist.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setTitle("ABC");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (IOException ex)
+        {
+            Logger.getLogger(TunesViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @FXML
@@ -77,8 +93,19 @@ public class TunesViewController implements Initializable
     }
 
     @FXML
-    private void deletePlaylist(ActionEvent event)
+    private void deletePlaylist(ActionEvent event) throws Exception
     {
+        Playlist selectedPlaylist = Playlists.getSelectionModel().getSelectedItem();
+        if (selectedPlaylist != null)
+        {
+            try
+            {
+                tModel.deletePlaylist(selectedPlaylist);
+            } catch (IOException ex)
+            {
+                Logger.getLogger(TunesViewController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     @FXML
