536aa9e084c3289fc3dea4252a34b01a21abfd49
msejerup <52162072+msejerup@users.noreply.github.com>
2019-12-02 10:35:08 +0100

    tilpasning

--- src/mytunes_group4/dal/database/SongDBDAO.java
+++ src/mytunes_group4/dal/database/SongDBDAO.java
@@ -15,6 +15,7 @@ import java.sql.SQLException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import mytunes_group4.dal.ISongDAO;
 import mytunes_group4.be.Song;
 import mytunes_group4.dal.DalException;
 
@@ -22,8 +23,8 @@ import mytunes_group4.dal.DalException;
  *
  * @author M
  */
-public class SongDBDAO
-{
+public class SongDBDAO implements ISongDAO{
+    
 
     private DatabaseConnector dbCon;
     
@@ -54,8 +55,10 @@ public class SongDBDAO
                 String songName = rs.getString("songName");
                 String artistName = rs.getString("artistName");
                 String genre = rs.getString("genre");
-                
-                Song son = new Song(artistName, songName, genre);
+                int songId = rs.getInt("songId");
+                double duration = rs.getDouble("duration");
+                String path = rs.getNString("path");
+                Song son = new Song(songId, artistName, songName, genre, duration, path);
                 allSongs.add(son);
             }
             
@@ -69,14 +72,58 @@ public class SongDBDAO
         
     }
     
-    public void deleteSong(Song song)
-    {
-        
+    @Override
+    public void deleteSong(Song song) throws DalException {
+        try ( Connection con = dbCon.getConnection()) {
+            int id = song.getId();
+            String sql = "DELETE FROM movie WHERE id=?;";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows != 1) {
+                throw new DalException();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new DalException();
+        }
     }
-    
-    public void editSong(Song song)
-    {
-        
+
+    @Override
+    public void updateSong(Song song) throws DalException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    @Override
+    public void writeAllSongs(List<Song> allSongs, String fileName) throws DalException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Song createSong(String artistName, String songName, String genre, double duration, String path) throws DalException {
+        try ( Connection con = dbCon.getConnection()) {
+            String sql = "INSERT INTO movie VALUES (?,?);";
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, artistName);
+            ps.setString(2, songName);
+            ps.setString(3, genre);
+            ps.setDouble(4, duration);
+            ps.setString(5, path);
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows == 1) {
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) // <-- Remember to do this!!
+                {
+                    int id = rs.getInt(1);
+                    Song mov = new Song(id, artistName, songName, genre, duration, path);
+                    return mov;
+                }
+            }
+            throw new DalException();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new DalException();
+        }
+    }
+   
 }
